c45e7a9991cac214ba6d6a6a49ee89ee217f747a
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-05 15:16:46 -0600

    Clean Python code, Markdown
    Add docstrings and comments to `aurora.py`.
    
    Format `aurora.py` in accordance with style from
    Hypatia Engine, e.g., whitespace, variable names.
    
    Simplify the methods: `is_user_message`, `is_join`, `run`.
    
    Clean/fix formatting in `README.md`.

--- Aurora.py
+++ Aurora.py
@@ -1,59 +1,107 @@
-from slackclient import SlackClient
 import json
 import os
 import time
 
+from slackclient import SlackClient
+
+
+# Slack Client Constants
 API_KEY=""
 USER=""
 NAME="Aurora"
 
+
 class Aurora(SlackClient):
+    """The Slack bot, Aurora, itself.
+
+    A Slack client is basically a user in a Slack
+    team chat. Aurora gives this "Slack Client,"
+    special responses, abilities, e.g., sending
+    an introduction message to new users.
+
+    """
+
     def __init__(self, key, user, name):
+        """Set Aurora's API key, ..
+
+        Args:
+            key (str): Slack API key
+            user (str):
+            name (str):
+
+        """
+
         SlackClient.__init__(self, key)
         self.user = user
         self.name = name
 
-    # Checks whether the given event is a message, and isn't from the bot.
-    def is_user_message(self, ev):
-        if "type" in ev and ev['type'] == "message" and ev['user'] != self.user:
-            return True
-        else:
-            return False
+    def is_user_message(self, event):
+        """Check whether the given event is a message
+        and isn't from the bot.
 
-    # Checks whether the given event was a channel join event.
-    def is_join(self, ev):
-        if "subtype" in ev and ev['subtype'] == "group_join":
-            return True
-        else:
-            return False
+        Args:
+            event (dict):
 
-    # Decodes bytes to utf-8 and then decodes the string from json.
+        """
+
+        return event.get("type") == "message" and event["user"] != self.user
+
+    def is_join(self, event):
+        """Check whether given event has a channel join event.
+
+        Args:
+            event:
+
+        """
+
+        return event.get("subtype") == "group_join"
+
     def unjson(self, bstring):
+        """Decodes bytes to UTF-8 and then decode the string
+        from JSON.
+
+        Args:
+            bstring:
+
+        """
+
         return json.loads(bstring.decode("utf_8"))
 
-    # Greet a new user with a prewritten paragraph.
-    def greet(self, ev):
-        user_id = self.unjson(self.api_call("users.info", user = ev['user']))
+    def greet(self, event):
+        """Greet new user with prewritten paragraph.
+
+        Args:
+            event (dict):
+
+        """
+
+        user_id = self.unjson(self.api_call("users.info", user=event['user']))
         username = user_id['user']['name']
+
         try:
             path = os.path.dirname(os.path.realpath(__file__)) + "/greeting.txt"
             paragraph = open(path).read().format(username, self.name)
-            self.rtm_send_message(ev['channel'], paragraph) 
+            self.rtm_send_message(event['channel'], paragraph) 
+
         except FileNotFoundError:
             print("Greeting file not found! {}".format(path))
 
     def run(self):
-        if self.rtm_connect():
-            while True:
-                response = self.rtm_read()
-    
-                for event in response:
-                    if self.is_join(event):
-                        self.greet(event) 
 
-                time.sleep(1)
-        else:
+        if not self.rtm_connect():
             print("Connection to RTM failed.")
+
+            return None
+
+        while True:
+            response = self.rtm_read()
+
+            for event in response:
+                if self.is_join(event):
+                    self.greet(event) 
+
+            time.sleep(1)
+
 
 aurora = Aurora(API_KEY, USER, NAME)
 aurora.run()
