797dffcd9c5f2873b5082cfca2a6d77db17115fc
Lily Lemmer <lily.lemmer@ave81.com>
2016-01-08 03:18:48 -0600

    Cleanup, apikey goes in ini, add requirements file
    You can use `bot_config.ini` to set the Slack bot API key.
    
    Add `requirements.txt` for installing requirements with `pip`.
    
    Update `README.md` to reflect changes.

--- Aurora.py
+++ Aurora.py
@@ -1,9 +1,14 @@
 from slacker import *
 from time import sleep
 
-API_KEY=""
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
 
 class Bot(Slacker):
+
     def __init__(self, key):
         """Set bot's API key and load userlist.
 
@@ -15,13 +20,22 @@ class Bot(Slacker):
         self.userlist = self.load_userlist()
 
     def load_userlist(self):
-        """Load userlist from textfile 'userlist'."""
+        """Load userlist from textfile 'userlist'.
+        
+        Returns:
+            set: The user IDs read from userlist file.
+
+        """
 
         try:
+
             with open("userlist", "r") as userlist:
+
                 return set(uid.strip() for uid in userlist.readlines())
+
         except FileNotFoundError:
             print("File not found... using empty userlist.")
+
             return set()
 
     def learn_users(self, uids):
@@ -30,9 +44,11 @@ class Bot(Slacker):
 
         Args:
             uids (set): Set of IDs to append (strs).
+
         """
 
         with open("userlist", "a") as userlist:
+
             for user in uids:
                 userlist.write("{}\n".format(user))
 
@@ -44,6 +60,7 @@ class Bot(Slacker):
         Args:
             uid (str): User ID to open a channel for.
             callback (function): Function to pass the channel ID to.
+
         """
 
         channel = self.im.open(uid)
@@ -55,11 +72,13 @@ class Bot(Slacker):
 
         Args:
             channel (str): Channel to send to.
+
         """
 
         self.chat.post_message(channel, "<3 This bot loves you <3", as_user=True)
 
     def run(self):
+
         while True:
             response = self.users.list()
             users = set(user["id"] for user in response.body["members"])
@@ -68,7 +87,9 @@ class Bot(Slacker):
                 print("No users defined. Learning users.")
                 self.learn_users(users)
                 print("Done.")
+
             else:
+
                 for user in (users ^ self.userlist):
                     self.with_channel(user, self.send_test)
                     print("Learning {}...".format(user))
@@ -77,5 +98,11 @@ class Bot(Slacker):
             self.userlist = users
             sleep(1)
 
-bot = Bot(API_KEY)
-bot.run()
+
+if __name__ == "__main__":
+    bot_config = configparser.ConfigParser()
+    bot_config.read("bot_config.ini")
+    api_key = bot_config.get("bot", "apikey")
+
+    bot = Bot(api_key)
+    bot.run()
--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# slack-bot-aurora
+# Slack Bot Aurora
 
-Greets new comers and performs other functions for HSO Slack team chat.
+This bot sends a private message (defined in a text file) to a user
+that's new to your team!
 
 ## Usage
 
 `greeting.txt` contains the text sent to new users. It's modified with
 `format` to include the user's username and the bot's name.
 
+A file (`userfile`) is used as a database for detecting new users. It
+is updated when new users are detected.
+
 # Dependencies
+
+`pip install -r requirements.txt`
 
   * `slacker`
